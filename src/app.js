a3a00ea3b7e9161378b2282fc1b91bb44fe09b50
sxcheng <1032211967@qq.com>
2018-10-30 11:40:03 +0800

    添加项目结构

--- src/app.js
+++ src/app.js
@@ -1,5 +1,39 @@
 import Koa from 'koa';
-
+import bodyParser from 'koa-bodyparser'; //表单解析
+import session from 'koa-session-minimal';
+import MysqlStore from 'koa-mysql-session';
+import Router from 'koa-router';
+import cors from 'koa2-cors';
+import config from './config/default';
+const path = require('path');
 const app = new Koa();
+const router = new Router();
 
+//session存储配置
+const sessionMysqlConfig = {
+  user: config.database.USERNAME,
+  password: config.database.PASSWORD,
+  database: config.database.DATABASE,
+  host: config.database.HOST
+}
 
+//配置session中间件
+app.use(session({
+  key: 'USER_SID',
+  store: new MysqlStore(sessionMysqlConfig)
+}))
+
+//跨域
+app.use(cors());
+
+router.get('/login', async(ctx) => {
+  let query = ctx.query;
+  ctx.body = {
+    query
+  }
+});
+
+app.use(router.routes())
+    .use(router.allowedMethods());
+
+app.listen(9000);
---
+++ src/config/default.js
@@ -0,0 +1,12 @@
+const config = {
+  port: 9000,
+  database: {
+    DATABASE: 'vr_server',
+    USERNAME: 'root',
+    PASSWORD: '123456',
+    PORT: '3306',
+    HOST: 'localhost'
+  }
+}
+
+export default config;
